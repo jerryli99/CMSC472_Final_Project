dd0fee30d2779062a5f2693c99bcd0ab5b2502cc
MaskSenpai <guessmana2@gmail.com>
2024-11-23 14:54:45 -0500

    resnet50 complete

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 python train.py \
-    --model_name resnet18 \
+    --model_name resnet50 \
     --num_classes 11 \
-    --train_data "/path/to/train" \
-    --val_data "/path/to/val" \
-    --test_data "/path/to/test" \
+    --train_data "..\photos\train" \
+    --val_data "..\photos\validation" \
+    --test_data "..\photos\test" \
     --pretrained
